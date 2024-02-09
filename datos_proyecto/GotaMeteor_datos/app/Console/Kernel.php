24ee5cbe965c4900d0180cadea78d477d298ebf7
root <root@HZ114474.H012108.NET>
2024-02-09 09:47:15 +0100

    inserción datos al momento

--- datos_proyecto/GotaMeteor_datos/app/Console/Kernel.php
+++ datos_proyecto/GotaMeteor_datos/app/Console/Kernel.php
@@ -12,6 +12,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
+        $schedule->command('app:alimentar-base');
         $schedule->command('app:alimentar-base')->everyFifteenMinutes();
         $schedule->command('app:random')->everyFifteenSeconds();
     }
